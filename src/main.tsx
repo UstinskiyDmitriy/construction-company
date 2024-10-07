07e245358e8464e63d7b933952b165868414f2f3
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-10-07 17:40:43 +0300

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import App from './components/app/App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.ts'
-import { HashRouter } from 'react-router-dom'
-
 createRoot(document.getElementById('root')!).render(
-  <HashRouter basename='/'>
+  
   <Provider store={store}>
     <App />
   </Provider>
-  </HashRouter>
+
 )
