7a1ca8e5a9d3c0ed34e3a00b991ec1052cd87b32
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-10-07 18:33:01 +0300

    changed path image

--- src/components/request-form/RequestForm.tsx
+++ src/components/request-form/RequestForm.tsx
@@ -9,7 +9,7 @@ export default function RequestForm() {
         <FormContacts 
           formText={`Задайте нам интересующий вас вопрос - наши архитекторы ответят вам`}
           formTitle={'Мы готовы воплотить вашу идею в жизнь'}
-          imageUrl={'./src/public/helmet.png'} 
+          imageUrl={'/helmet.png'} 
           closeModal={() => console.log()}        
         />
       </div>
